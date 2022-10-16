4a787dbce0b067683b850bf7fb6d787544006f3e
Abhishek Singh <meabhisingh25@gmail.com>
2022-10-17 00:09:28 +0530

    more responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -79,27 +79,27 @@ const MyCarousel = () => (
     showArrows={false}
   >
     <Box w="full" h={'100vh'}>
-      <Image src={img1} />
+      <Image src={img1} h="full" w={'full'} objectFit={'cover'} />
       <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
         Watch The Future
       </Heading>
     </Box>
     <Box w="full" h={'100vh'}>
-      <Image src={img2} />
+      <Image src={img2} h="full" w={'full'} objectFit={'cover'} />
       <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
         Future is Gaming
       </Heading>
     </Box>
 
     <Box w="full" h={'100vh'}>
-      <Image src={img3} />
+      <Image src={img3} h="full" w={'full'} objectFit={'cover'} />
       <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
         Gaming on Console
       </Heading>
     </Box>
 
     <Box w="full" h={'100vh'}>
-      <Image src={img4} />
+      <Image src={img4} h="full" w={'full'} objectFit={'cover'} />
       <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
         Night life is cool
       </Heading>
